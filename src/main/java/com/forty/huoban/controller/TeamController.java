986e3db76e7e12503510e40cebcf8838ba21e60a
四十不是拾肆 <1814096199@qq.com>
2025-02-04 14:52:29 +0800

    新增Team相关接口文件

--- src/main/java/com/forty/huoban/controller/TeamController.java
+++ src/main/java/com/forty/huoban/controller/TeamController.java
@@ -1,10 +1,13 @@
 package com.forty.huoban.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author: FortyFour
  * @description:
  * @time: 2025/2/4 14:37
  * @version:
  */
+@RestController
 public class TeamController {
 }
